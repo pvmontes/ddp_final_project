2037e57b86c23a9df82a743edaac06e027c312bb
Puri Velasco <m.purificacion.velasco@udc.es>
2019-12-11 14:14:06 +0100

    Added all of the fles

--- publicacion.R
+++ publicacion.R
@@ -1,4 +1,11 @@
 publish_github("pvmontes","ddp_final_project")
 
 
+# publish_rpubs("Developing data products final project", html_file = "index.html")
 
+
+publish("Developing data products final project", html_file = "index.html", host="rpubs")
+
+
+
+getwd()
